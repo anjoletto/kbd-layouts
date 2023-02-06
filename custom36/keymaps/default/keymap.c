9935e1a48952c221467e5ca556c91f2f236436a0
Leonardo Anjoletto Ferreira <43471239+anjoletto@users.noreply.github.com>
2023-02-05 23:07:26 -0300

    refac: ralt and media keys

--- custom36/keymaps/default/keymap.c
+++ custom36/keymaps/default/keymap.c
@@ -12,7 +12,9 @@ enum layers {
 
 #define L_CTRL  MO(_CONTROLS)
 #define CL_SPC  LT(_COMMS, KC_SPC)
-#define OS_RALT OSM(MOD_RALT)
+
+#define RAT_DOT MT(MOD_RALT, KC_DOT)
+#define LAT_COM MT(MOD_LALT, KC_COMM)
 
 // overrides
 const key_override_t colon_override = ko_make_basic(MOD_MASK_SHIFT, KC_COLN, KC_SCLN);
@@ -33,36 +35,36 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    //|--------+--------+--------+--------+--------|          |--------+--------+--------+--------+--------|
       KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,               KC_J,    KC_L,    KC_U,    KC_Y,    KC_COLN,
       KC_A,    KC_R,    KC_S,    KC_T,    KC_D,               KC_H,    KC_N,    KC_E,    KC_I,    KC_O,
-      KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,               KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,
+      KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,               KC_B,    KC_K,    KC_M,    LAT_COM, RAT_DOT,
    //|--------+--------+--------+--------+--------|          |--------+--------+--------+--------+--------|
                         TG_MIC,  L_SYMS,  KC_LGUI,            L_CTRL,  CL_SPC,  KC_LSFT
    //                  |--------+--------+--------|          |--------+--------+--------|
     ),
     [_SYMBOLS] = LAYOUT_split_3x5_3(
    //|--------+--------+--------+--------+--------|          |--------+--------+--------+--------+--------|
-      KC_TILD, KC_PERC, KC_HASH, KC_DLR,  XXXXXXX,            XXXXXXX, KC_CIRC, KC_ASTR, KC_MINS, KC_DEL,
+      KC_TILD, KC_HASH, KC_DLR,  KC_PERC, XXXXXXX,            XXXXXXX, KC_CIRC, KC_ASTR, KC_MINS, KC_DEL,
       KC_BSLS, KC_LCBR, KC_LBRC, KC_LPRN, KC_LT,              KC_GT,   KC_RPRN, KC_RBRC, KC_RCBR, KC_SLSH,
       KC_EXLM, KC_PIPE, KC_UNDS, KC_QUOT, KC_GRV,             XXXXXXX, KC_DQUO, KC_EQL,  KC_PLUS, KC_QUES,
    //|--------+--------+--------+--------+--------|          |--------+--------+--------+--------+--------|
-                        XXXXXXX, _______, _______,            _______, _______, OS_RALT
+                        XXXXXXX, _______, _______,            _______, _______, _______
    //                  |--------+--------+--------|          |--------+--------+--------|
     ),
     [_COMMS] = LAYOUT_split_3x5_3(
    //|--------+--------+--------+--------+--------|          |--------+--------+--------+--------+--------|
-      KC_TAB,  KC_7,    KC_8,    KC_9,    XXXXXXX,            KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_BSPC,
-      KC_ESC,  KC_4,    KC_5,    KC_6,    XXXXXXX,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,
-      KC_LCTL, KC_1,    KC_2,    KC_3,    XXXXXXX,            XXXXXXX, KC_LALT, XXXXXXX, KC_PSCR, KC_LSFT,
+      KC_TAB,  KC_7,    KC_8,    KC_9,    KC_MUTE,            KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_BSPC,
+      KC_ESC,  KC_4,    KC_5,    KC_6,    KC_VOLU,            KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ENT,
+      KC_LCTL, KC_1,    KC_2,    KC_3,    KC_VOLD,            KC_MPRV, KC_MPLY, KC_MNXT, KC_PSCR, KC_LSFT,
    //|--------+--------+--------+--------+--------|          |--------+--------+--------+--------+--------|
                         XXXXXXX, KC_0,    _______,            _______, _______, _______
    //                  |--------+--------+--------|          |--------+--------+--------|
     ),
     [_CONTROLS] = LAYOUT_split_3x5_3(
    //|--------+--------+--------+--------+--------|          |--------+--------+--------+--------+--------|
-      KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX,            XXXXXXX, KC_MNXT, XXXXXXX,   XXXXXXX,   XXXXXXX,
-      KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX,            XXXXXXX, KC_MPLY, XXXXXXX,   XXXXXXX,   XXXXXXX,
-      KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX,            XXXXXXX, KC_MPRV, XXXXXXX,   XXXXXXX,   XXXXXXX,
+      KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+      KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX,            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
    //|--------+--------+--------+--------+--------|          |--------+--------+--------+--------+--------|
-                        QK_BOOT, _______, _______,            _______, _______, _______
+                        XXXXXXX, _______, QK_BOOT,            _______, _______, _______
    //                  |--------+--------+--------|          |--------+--------+--------|
     ),
 };
